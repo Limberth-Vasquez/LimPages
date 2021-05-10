90db2b9f308570486b17e34fe09dc57da8249d80
Limberth Vasquez Quesada <lvasquez@tecnosys.net>
2021-05-10 12:42:35 -0600

    bot test offonline

---
+++ bots/oficinaOnlineCr/precache-manifest.5f40c4143a13a6728be991547652af9e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "572675442266b6cb7e804c4defbf6cc1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "88fa5b4bb118e9063775",
+    "url": "./static/css/main.beb28c0e.chunk.css"
+  },
+  {
+    "revision": "e9b98756ec0ddd0a2a08",
+    "url": "./static/js/2.c814e292.chunk.js"
+  },
+  {
+    "revision": "4563017d3783b0506315b1de43d1ae90",
+    "url": "./static/js/2.c814e292.chunk.js.LICENSE"
+  },
+  {
+    "revision": "88fa5b4bb118e9063775",
+    "url": "./static/js/main.d40bdbe6.chunk.js"
+  },
+  {
+    "revision": "4bf492bf316ec3539a4e",
+    "url": "./static/js/runtime-main.a24eb5d8.js"
+  },
+  {
+    "revision": "866b5597d7c58a5142bba8a36a300791",
+    "url": "./static/media/BG.866b5597.JPG"
+  }
+]);
