45906f9e044653aa4748eadae0c3ad97957df095
Limberth Vasquez Quesada <lvasquez@tecnosys.net>
2021-06-04 23:48:54 -0600

    bot limba ignacio

---
+++ bots/Limba/precache-manifest.a9d6806cc14d3b9f17493bd141d7a5d8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a9b40b713c33cf0e079224fbf50ce63",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e59a80c27ac306983bc2",
+    "url": "./static/css/main.beb28c0e.chunk.css"
+  },
+  {
+    "revision": "037373bcf9631fcb3e5d",
+    "url": "./static/js/2.b08ac8f8.chunk.js"
+  },
+  {
+    "revision": "4563017d3783b0506315b1de43d1ae90",
+    "url": "./static/js/2.b08ac8f8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e59a80c27ac306983bc2",
+    "url": "./static/js/main.c5e41be7.chunk.js"
+  },
+  {
+    "revision": "4bf492bf316ec3539a4e",
+    "url": "./static/js/runtime-main.a24eb5d8.js"
+  },
+  {
+    "revision": "bc594eefbce473d10bd99d2aaba140ff",
+    "url": "./static/media/backgroundUNA.bc594eef.JPG"
+  }
+]);
